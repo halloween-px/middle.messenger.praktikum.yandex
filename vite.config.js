13e63d93fbbeeadb9facb5f4bbfa9751be24b16c
Ruslan_Kor <koryakhovruslan9498@mail.ru>
2024-10-10 20:11:56 +0300

    up

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import handlebars from 'vite-plugin-handlebars'
 
 export default defineConfig({
   plugins: [handlebars()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/main.scss";`
+      }
+    }
+  }
 })
