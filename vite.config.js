9c78ffa39bc199b22c599ba6ff4680729f0011b7
Ruslan_Kor <koryakhovruslan9498@mail.ru>
2025-02-12 23:34:51 +0300

    sprint_4

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars'
 import { resolve } from 'path'
 
+import { webcrypto } from 'node:crypto'
+
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto
+}
+
 export default defineConfig({
   server: {
     port: 3000,
